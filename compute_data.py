5dccd0beaf32a05f449903bff85f0fcb124f9736
Bertrand Thierry <bthierry@math.cnrs.fr>
2023-06-05 11:19:00 -0500

    timefrequency is set to 2*dt

--- compute_data.py
+++ compute_data.py
@@ -93,5 +93,5 @@ data["Tmax"]       = Tmax
 
 # write data
 
-with open(output_file, 'w', encoding='utf-8') as f:
+with open(filename, 'w', encoding='utf-8') as f:
   json.dump(data, f, ensure_ascii=False, indent=4)
