1a6cc1646fb5679c61fd29a5f006fdb5c48c3d18
Bertrand Thierry <bthierry@math.cnrs.fr>
2023-06-09 13:26:47 -0500

    Tmax: decimal limit

--- compute_data.py
+++ compute_data.py
@@ -65,7 +65,7 @@ Tn = min_vert_dist / vn # time to reach top or bottom border
 Tmax = np.around(np.minimum(Th, Tn), decimals = 3)
 Tmax = np.minimum(Tmax, 1)
 # round from dt
-Tmax = int(Tmax/dt)*(dt+1)
+Tmax = np.around(Tmax/dt,decimals = 4)*(dt+1)
 print('Tmax = ', Tmax)
 
 # Number of hexa in each dimension
