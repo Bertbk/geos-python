7a12f60dac94c2d8ce05148452dd760d90a8430b
Bertrand Thierry <bthierry@math.cnrs.fr>
2023-06-14 10:08:48 -0500

    plotres

--- 06_plotres.py
+++ 06_plotres.py
@@ -7,10 +7,9 @@
 import argparse
 import json
 import numpy as np
-import scipy.ndimage as nd
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
-from matplotlib.widgets import Slider
+rcParams['font.size'] = 10
 
 # get data
 parser = argparse.ArgumentParser(description='Cut a hdf5 file using JSON data. Warning: override of JSON file')
@@ -19,7 +18,7 @@ args = parser.parse_args()
 jsonfile = args.json
 
 # get data
-with open(jsonfile) as f:
+with open(jsonfile, mode="r") as f:
   data = json.load(f)
 
 dt = data["dt_hdf5"]
@@ -34,6 +33,14 @@ ymax = data["ymax"]
 zmin = data["zmin"]
 zmax = data["zmax"]
 
+sigma = data["sigma"]
+
+nx = data["nx_elem"] + 1
+ny = data["ny_elem"] + 1
+nz = data["nz_elem"] + 1
+
+dt=data["dt_hdf5"]
+
 xscale = np.linspace(xmin, xmax, num =nx, endpoint=True)
 yscale = np.linspace(ymin, ymax, num =ny, endpoint=True)
 zscale = np.linspace(zmin, zmax, num =nz, endpoint=True)
@@ -42,67 +49,25 @@ zscale = np.linspace(zmin, zmax, num =nz, endpoint=True)
 pyz = np.load("pyz.npy")
 pxz = np.load("pxz.npy")
 pxy = np.load("pxy.npy")
+print("pyz.shape = " + str(pyz.shape))
+ndt = pyz.shape[0]
 
-# Matplotlib inline
+# Save fig
+time_target = 0.82 # in seconds
+it = np.minimum(int(time_target/dt), ndt-2)
+time = it*dt
+vmax=np.percentile(np.abs(pxz[it,::]), 99.5)
 
-# max value (for scaling)
-value_max = np.percentile(np.abs(pxy),99.2)
+fig, ax = plt.subplots()
+pos = ax.imshow(np.transpose(pxz[it,:,:]),vmin=-vmax,vmax=vmax,cmap='seismic', extent=[xmin, xmax, zmin, zmax])
+ax.tick_params('both', length=2, width=0.5, which='major',labelsize=10)
+ax.set_title("GEOS: Wavefield at t="+str(format(time*1000., '.2f'))+"ms with sigma="+str(sigma),fontsize=10)
+ax.set_xlabel("X Coordinate (m)",fontsize=10)
+ax.set_ylabel("Z Coordinate (m)",fontsize=10)
+ax.grid()
+fig.colorbar(pos, ax=ax)
 
-# ixmin = 1
-# ixmax = nx-1
-# iymin = 1
-# iymax = ny-1
-# izmin = 1
-# izmax = nz-1
-
-# Function for widget
-def plotWfld(dyz,dxz,dxy,fig, ax, it,output="figure",save=False):
-  time=it*dt
-  ax[0].imshow(np.transpose(dyz[it,:,:]),vmin=-value_max,vmax=value_max,cmap='seismic')
-  ax[0].grid()
-  ax[0].set_xlabel("Y")
-  ax[0].set_ylabel("Z")
-  ax[0].set_title(r"X-slice @ %0.2f sec" %time)
-  ax[0].set_xlim([ymin, ymax])
-  ax[0].set_ylim([zmin, zmax])
-  ax[1].imshow(np.transpose(dxz[it,:,:]),vmin=-value_max,vmax=value_max,cmap='seismic')
-  ax[1].grid()
-  ax[1].set_xlabel("X")
-  ax[1].set_ylabel("Z")
-  ax[1].set_title(r"Y-slice @ %0.2f sec" %time)
-  ax[1].set_xlim([xmin, xmax])
-  ax[1].set_ylim([zmin, zmax])
-  ax[2].imshow(np.transpose(dxy[it,:,:]),vmin=-value_max,vmax=value_max,cmap='seismic')
-  ax[2].grid()
-  ax[2].set_xlabel("X")
-  ax[2].set_ylabel("Y")
-  ax[2].set_title(r"Z-slice @ %0.2f sec" %time)
-  ax[2].set_xlim([xmin, xmax])
-  ax[2].set_ylim([ymin, ymax])
-
-#  fig.tight_layout()
-  if save==True:
-      plt.savefig("./fig/"+output+"_%d" %it)
-
-#plt.figure(figsize=(16,6))
-fig, ax = plt.subplots(1,3)
-fig.set_figheight(8)
-fig.set_figwidth(16)
-
-# Slider
-ax_time = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-
-time_slide = Slider(
-  ax_time, "Iteration", 0, ndt,
-  valinit=1, valstep=1,
-  initcolor='none'  # Remove the line marking the valinit position.
-)
-
-def update(val):
-    current_time = time_slide.val
-    plotWfld(pyz,pxz,pxy,fig,ax,current_time,output="figure",save=False)
-    fig.canvas.draw_idle()
-
-plotWfld(pyz,pxz,pxy,fig, ax, int(ndt/2),output="figure",save=False)
-time_slide.on_changed(update)
-plt.show()
+figtitle = "geos-sigma"+str(sigma)+".png"
+print("Saving figure as ... " + figtitle)
+fig.savefig(figtitle)
+print("done")
